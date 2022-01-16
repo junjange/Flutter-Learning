a8078efaf767fcfdae02b33ddfa9806a29776924
junjange <wnswkd486@naver.com>
2022-01-16 17:16:22 +0900

    image_picker

---
+++ camera/lib/camera_ex.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraExample extends StatefulWidget {
+  const CameraExample({Key? key}) : super(key: key);
+
+  @override
+  _CameraExampleState createState() => _CameraExampleState();
+}
+
+class _CameraExampleState extends State<CameraExample> {
+  File? _image;
+  final picker = ImagePicker();
+
+  // 비동기 처리를 통해 카메라와 갤러리에서 이미지를 가져온다.
+  Future getImage(ImageSource imageSource) async {
+    final image = await picker.pickImage(source: imageSource);
+
+    setState(() {
+      _image = File(image!.path); // 가져온 이미지를 _image에 저장
+    });
+  }
+
+  // 이미지를 보여주는 위젯
+  Widget showImage() {
+    return Container(
+        color: const Color(0xffd0cece),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        child: Center(
+            child: _image == null
+                ? Text('No image selected.')
+                : Image.file(File(_image!.path))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 화면 세로 고정
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return Scaffold(
+        backgroundColor: const Color(0xfff4f3f9),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text(
+            //   'Classify',
+            //   style: TextStyle(fontSize: 25, color: const Color(0xff1ea271)),
+            // ),
+            SizedBox(height: 25.0),
+            showImage(),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // 카메라 촬영 버튼
+                FloatingActionButton(
+                  child: Icon(Icons.add_a_photo),
+                  tooltip: 'pick Iamge',
+                  onPressed: () {
+                    getImage(ImageSource.camera);
+                  },
+                ),
+
+                // 갤러리에서 이미지를 가져오는 버튼
+                FloatingActionButton(
+                  child: Icon(Icons.wallpaper),
+                  tooltip: 'pick Iamge',
+                  onPressed: () {
+                    getImage(ImageSource.gallery);
+                  },
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
