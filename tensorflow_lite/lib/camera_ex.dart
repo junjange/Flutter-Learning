4ce69e66c53797edb9183f434c19243cc898389e
junjange <wnswkd486@naver.com>
2022-01-18 16:09:18 +0900

    tflite

---
+++ tensorflow_lite/lib/camera_ex.dart
@@ -0,0 +1,202 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+class CameraExample extends StatefulWidget {
+  const CameraExample({Key? key}) : super(key: key);
+
+  @override
+  _CameraExampleState createState() => _CameraExampleState();
+}
+
+class _CameraExampleState extends State<CameraExample> {
+  File? _image;
+  final picker = ImagePicker();
+  List? _outputs;
+
+  // 앱이 실행될 때 loadModel 호출
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  // 모델과 label.txt를 가져온다.
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/testlabel.txt",
+    ).then((value) {
+      setState(() {
+        //_loading = false;
+      });
+    });
+  }
+
+  // 비동기 처리를 통해 카메라와 갤러리에서 이미지를 가져온다.
+  Future getImage(ImageSource imageSource) async {
+    final image = await picker.pickImage(source: imageSource);
+
+    setState(() {
+      _image = File(image!.path); // 가져온 이미지를 _image에 저장
+    });
+    await classifyImage(File(image!.path)); // 가져온 이미지를 분류 하기 위해 await을 사용
+  }
+
+  // 이미지 분류
+  Future classifyImage(File image) async {
+    print("asdasddas$image");
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 2, // defaults to 5
+        threshold: 0.2, // defaults to 0.1
+        asynch: true // defaults to true
+        );
+    setState(() {
+      _outputs = output;
+    });
+  }
+
+  // 이미지를 보여주는 위젯
+  Widget showImage() {
+    return Container(
+        color: const Color(0xffd0cece),
+        margin: EdgeInsets.only(left: 95, right: 95),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        child: Center(
+            child: _image == null
+                ? Text('No image selected.')
+                : Image.file(File(_image!.path))));
+  }
+
+  recycleDialog() {
+    _outputs != null
+        ? showDialog(
+            context: context,
+            barrierDismissible:
+                false, // barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+            builder: (BuildContext context) {
+              return AlertDialog(
+                // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      _outputs![0]['label'].toString().toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15.0,
+                        background: Paint()..color = Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  Center(
+                    child: new FlatButton(
+                      child: new Text("Ok"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  )
+                ],
+              );
+            })
+        : showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "데이터가 없거나 잘못된 이미지 입니다.",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  Center(
+                    child: new FlatButton(
+                      child: new Text("Ok"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  )
+                ],
+              );
+            });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 화면 세로 고정
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return Scaffold(
+        backgroundColor: const Color(0xfff4f3f9),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Classify',
+              style: TextStyle(fontSize: 25, color: const Color(0xff1ea271)),
+            ),
+            SizedBox(height: 25.0),
+            showImage(),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // 카메라 촬영 버튼
+                FloatingActionButton(
+                  child: Icon(Icons.add_a_photo),
+                  tooltip: 'pick Iamge',
+                  onPressed: () async {
+                    await getImage(ImageSource.camera);
+                    recycleDialog();
+                  },
+                ),
+
+                // 갤러리에서 이미지를 가져오는 버튼
+                FloatingActionButton(
+                  child: Icon(Icons.wallpaper),
+                  tooltip: 'pick Iamge',
+                  onPressed: () async {
+                    await getImage(ImageSource.gallery);
+                    recycleDialog();
+                  },
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+
+  // 앱이 종료될 때
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
